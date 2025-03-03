0707c328e8c3e364023182611c5febe000f18e6c
IncharaNShankar <160014618+IncharaNShankar@users.noreply.github.com>
2025-03-03 15:08:49 +0530

    Update prg1.java

--- prg1.java
+++ prg1.java
@@ -2,7 +2,8 @@ class Main {
 public static void main(String[] args) { 
     int first = 10;
     int second = 20;
-    int sum = first + second;
-    System.out.println(first + " + " + second + " = "  + sum);
+    int third = 30;
+    int sum = first + second + third;
+    System.out.println(first + " + " + second +  "+" + third + " = "  + sum);
   }
 }
